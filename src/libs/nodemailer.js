7dbdf416bdb05ad21300bfa586cac31dc9e15b66
GuilhermeNotFound404 <leonadokof123@gmail.com>
2023-11-17 08:50:36 -0300

    update

--- src/libs/nodemailer.js
+++ src/libs/nodemailer.js
@@ -1,13 +1,13 @@
 import nodemailer from 'nodemailer';
-// basicamente quando tu da esse comando, ele serve para conectado em um servido de e-mail, isso demora uns 5 segundos pra mais
-// na vercel a versão gratuita limita em requisicões de até 10 segundos, se demorar mais que isso sua requisição da erro, por isso geralmente não se conecta usando servidor de e-mail usando serverless
 export const transporter = nodemailer.createTransport({
   host: "smtp.office365.com",
   port: 587,
   auth: {
     user: "guilherme.souza617@etec.sp.gov.br",
     pass: "Chesterbennington123"
-  }
+  },
+  pool: true, // Habilitar pooling
+  connectionTimeout: 5000
 });
 
 export const promisifySendMail = async (mailOptions) => {
